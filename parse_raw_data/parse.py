38c092e396d565706c5cb926dd6328b81247f632
gamijin0 <497386856@qq.com>
2018-03-07 17:52:13 +0800

    implement a elegance way to catch the stop signal

--- parse_raw_data/parse.py
+++ parse_raw_data/parse.py
@@ -1,9 +1,10 @@
 import threading
 import queue
+import signal
 
 
-BLOCK_QUEUE = queue.Queue(5)
-
+BLOCK_QUEUE = queue.Queue(8)
+STOP_WORK_FLAG = False
 
 #定义article的节点类
 class Article():
@@ -26,9 +27,17 @@ class Article():
 
 def worker(add_entity_func):
     while True:
+        if(STOP_WORK_FLAG is True and BLOCK_QUEUE.empty()):
+            break
         entity = BLOCK_QUEUE.get(block=True)
         add_entity_func(entity)
         BLOCK_QUEUE.task_done()
+
+def feeder(DBLP_DATA_FILE):
+    for entity in generate_entity_from_file(DBLP_DATA_FILE):
+        if(STOP_WORK_FLAG):
+            break
+        BLOCK_QUEUE.put(entity,block=True)
 
 def save_check_point(line_num):
     with open('last_line.txt', 'w') as outfile: outfile.write(str(line_num))
@@ -69,12 +78,9 @@ def process_block_add_entity(block_content):
 
 def generate_entity_from_file(DBLP_DATA_FILE):
     start_from = 0
-    try:
-        with open('last_line.txt', 'r') as llf:
-            start_from = int(llf.read())
-            print("Re-start from [%d] " % start_from)
-    except:
-        pass
+    with open('last_line.txt', 'r') as llf:
+        start_from = int(llf.read())
+        print("Re-start from [%d] " % start_from)
 
     block_content = []
 
@@ -89,14 +95,34 @@ def generate_entity_from_file(DBLP_DATA_FILE):
             else:
                 block_content.append(line)
 
+def stop_work(signum=None, frame=None):
+    global STOP_WORK_FLAG
+    STOP_WORK_FLAG = True
+    print("\n[Stopping now ...]\n")
+
+
+signal.signal(signal.SIGINT,stop_work)
+signal.signal(signal.SIGTERM,stop_work)
+
+
 def do_the_parse(DBLP_DATA_FILE,add_entity_func,thread_num=2):
+
+    worker_list = []
 
     for i in range(thread_num):
              t = threading.Thread(target=worker,args=(add_entity_func,))
+             worker_list.append(t)
              t.start()
-    print("Started %d workers." % thread_num)
 
-    for entity in generate_entity_from_file(DBLP_DATA_FILE):
-        BLOCK_QUEUE.put(entity,block=True)
+    feeder_thread = threading.Thread(target=feeder,args=(DBLP_DATA_FILE,))
+    feeder_thread.start()
 
+    print("Started %d workers and 1 feeder." % thread_num)
+
+    while True:
+        sig = input("\n================================================\n [  When you want to stop, please input 'stop'] \n================================================\n\n")
+        if(sig=="stop"):
+            stop_work()
+            break
     BLOCK_QUEUE.join()
+    exit()
